3d1e87b1bd5371105f3be7498b3a03864e886635
Yoann Roulé <yoann.roule@gmail.com>
2017-12-07 14:34:51 +0100

    deleting some folders

--- views/components/createAccount/create-account-component.js
+++
@@ -1,4 +0,0 @@
-angular.module('myApp').component('createAccountComponent', {
-  templateUrl: 'views/components/createAccount/create-account-view.html',
-  controller: 'createAccountController'
-});
--- views/components/login/login-component.js
+++
@@ -1,4 +0,0 @@
-angular.module('myApp').component('loginComponent', {
-  templateUrl: 'views/components/login/login-view.html',
-  controller: 'loginController'
-});
