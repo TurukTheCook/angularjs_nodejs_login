1056b3ee7d4642cde8406cf54ddeb9f824dc5d0c
Yoann Roulé <yoann.roule@gmail.com>
2017-12-07 17:31:04 +0100

    changes to promise with explaination

--- front-end/views/components/login/login-controller.js
+++ front-end/views/components/login/login-controller.js
@@ -12,13 +12,14 @@ function login() {
     password: $scope.password
   };
 
-  $http.post('http://localhost:1407/login', JSON.stringify(data)).then(function(res) {
-    if (res.status == 200) {
+  // Pour gerer les messages en cas d'erreur: then puis 2 fonctions :
+  // callbacks en cas de succes et en cas d'echec
+  $http.post('http://localhost:1407/login', JSON.stringify(data))
+    .then(function(res) {
       localStorage.setItem('auth-token', res.data.token);
       $scope.message = res.data.token;
-    } else {
-      $scope.message = res.data.message;
-    }
+  }, function(res){
+    $scope.message = res.data.message;
   });
 }
 
